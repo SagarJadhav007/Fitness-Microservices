296e4619ab4beac91fd06800e0dc070376ae8361
Sagar Jadhav <jadhavsagar1505@gmail.com>
2025-10-30 22:56:07 +0530

    Created User and Activity microservices

---
+++ activityservice/src/main/java/com/example/activityservice/model/ActivityType.java
@@ -0,0 +1,15 @@
+package com.example.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGHT_TRAINING,
+    YOGA,
+    CARDIO,
+    STRETCHING,
+    HIIT,
+    OTHER
+
+}
