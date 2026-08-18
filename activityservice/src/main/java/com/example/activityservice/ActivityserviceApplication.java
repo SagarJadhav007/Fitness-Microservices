9f9019cdedb9f9dc79df3e6668fedac7bade157a
SagarJadhav007 <jadhavsagar1505@gmail.com>
2026-08-18 22:09:36 +0530

    test: activityservice selective ci cd

--- activityservice/src/main/java/com/example/activityservice/ActivityserviceApplication.java
+++ activityservice/src/main/java/com/example/activityservice/ActivityserviceApplication.java
@@ -3,6 +3,7 @@ package com.example.activityservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//comment
 @SpringBootApplication
 public class ActivityserviceApplication {
 
